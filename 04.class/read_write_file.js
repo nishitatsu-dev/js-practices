a75416e3a43d5b4164f09aa73ba700f98edc2fde
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2023-06-18 15:22:48 +0900

    保存先ファイルをgit管理から除外

--- 04.class/read_write_file.js
+++ 04.class/read_write_file.js
@@ -6,7 +6,9 @@ export class ReadWriteFile {
   static async jsonToObject() {
     try {
       const memos = new ReadWriteFile();
-      const memos_json = fs.readFileSync("memos.json", { encoding: "utf8" });
+      const memos_json = fs.readFileSync("exclude/memos.json", {
+        encoding: "utf8",
+      });
       const minimum_length = 48;
       if (memos_json.length <= minimum_length) {
         memos.object = { dummy: { name: "仮メモ（内容無し）", value: "" } };
@@ -27,7 +29,7 @@ export class ReadWriteFile {
   save() {
     try {
       const memos_json = JSON.stringify(this.object, null, 2);
-      fs.writeFileSync("memos.json", memos_json);
+      fs.writeFileSync("exclude/memos.json", memos_json);
     } catch (e) {
       console.error(e.name + ": " + e.message);
       process.exitCode = 1;
